d34af8608d404fd5d2bb6ac79799e9144115f282
LucianoMazar <lmazaraojr@gmail.com>
2026-02-06 10:25:48 -0300

    finalização lógica do imc

--- bin/imc_calculator.dart
+++ bin/imc_calculator.dart
@@ -1,3 +1,29 @@
+import 'dart:io' show stdin, stdout;
+
+import 'package:imc_calculator/models/pessoa.dart';
+
 void main(List<String> arguments) {
-  print('Hello world');
+  String name;
+  double altura;
+  double peso;
+  Pessoa pessoa;
+
+  print('INICIANDO CALCULADORA DE IMC');
+  while (true) {
+    try {
+      stdout.write("Digite seu nome: ");
+      name = stdin.readLineSync() ?? '';
+      stdout.write("Digite sua altura(m): ");
+      altura = double.parse(stdin.readLineSync() ?? '0');
+      stdout.write("Digite seu peso(kg): ");
+      peso = double.parse(stdin.readLineSync() ?? '0');
+      pessoa = Pessoa(name, peso, altura);
+      break;
+    } catch (e) {
+      print("$e\nTente novamente");
+    }
+  }
+
+  print('PESSOA CADASTRADA COM SUCESSO');
+  print("O IMC de ${pessoa.nome} é ${pessoa.imc.toStringAsFixed(2)}(${pessoa.categoriaImc()})");
 }
--- lib/models/pessoa.dart
+++ lib/models/pessoa.dart
@@ -21,10 +21,30 @@ class Pessoa {
     _altura = altura;
   }
 
-  double get imc => _peso / pow(_altura, 2);
+  double get imc => (_peso / pow(_altura, 2));
 
   Pessoa(this.nome, double peso, double altura) {
     this.peso = peso;
     this.altura = altura;
+  }
+
+  String categoriaImc() {
+    if (imc < 16) {
+      return "Magreza Grave";
+    } else if (imc < 17) {
+      return "Magreza Moderada";
+    } else if (imc < 18.5) {
+      return "Magreza Leve";
+    } else if (imc < 25) {
+      return "Saudável";
+    } else if (imc < 30) {
+      return "Sobrepeso";
+    } else if (imc < 35) {
+      return "Obesidade grau I";
+    } else if (imc < 40) {
+      return "Obesidade grau II (Severa)";
+    } else {
+      return "Obesidade grau III (Mórbida)";
+    }
   }
 }
